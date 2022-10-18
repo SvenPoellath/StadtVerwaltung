1351bad5f091f26a9337ff5158904a722d48bd15
Moritz.Schley <moritz.schley@icloud.com>
2022-10-18 12:35:58 +0200

    Initial backened commit

---
+++ src/main/java/com/stadtverwaltung/pjms/PjmsApplication.java
@@ -0,0 +1,13 @@
+package com.stadtverwaltung.pjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjmsApplication.class, args);
+	}
+
+}
