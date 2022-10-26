fbd28a40c6df860d4ad380530bd7ed8660567891
SvenPoellath <sven.poellath@icloud.com>
2022-10-26 20:45:38 +0200

    Edited Map screen

--- src/components/pages/Maps.js
+++ src/components/pages/Maps.js
@@ -6,11 +6,21 @@ import {
     Marker,
     Popup
   } from 'react-leaflet'
+import { Button } from '../Button';
 
 
-
+ 
 
 export default function Map() {
+  const handleScrollDown = () => {
+    window.scrollTo( { top: 40000, behavior: 'smooth'})
+  }
+  function getData(url, cb) {
+    fetch(url)
+    .then(response => response.json ())
+    .then(result => cb(result));
+    }
+  var jsondata = getData('http://localhost:8080/cases', (jsondata))
   return (
     <div className='container'>
       <h1>
@@ -21,12 +31,41 @@ export default function Map() {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[49.4774, 8.4452]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+
+
     </MapContainer>
+    <button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+          onClick={handleScrollDown}
+        >
+          Weiter
+        </button>
+    <div className='form-box'>
+    <form className='Description-Form'>
+      <label>
+        Description
+        
+      </label>
+      <input type='text' placeholder='Enter Description'/>
+    </form>
+    </div>
+
+
     </div>
   );
 }
+/*      <div className="stock-container">
+        {jsondata.Map((data, key) => {
+          return(
+            <div key={key}>
+            <Marker position={[data.latitude, data.longitude]}>
+            <Popup>
+              {data.kindOfCase} <br /> {data.description}
+            </Popup>
+          </Marker>
+          </div>
+          );
+        })}
+      </div> */
