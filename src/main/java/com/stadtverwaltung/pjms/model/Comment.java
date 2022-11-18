051dd5b5aa93ce70ec4db193d72c5612f4fada25
Moritz Schley <moritz.schley@icloud.com>
2022-11-18 18:49:10 +0100

    Added WIP comments function (not working yet)

---
+++ src/main/java/com/stadtverwaltung/pjms/model/Comment.java
@@ -0,0 +1,9 @@
+package com.stadtverwaltung.pjms.model;
+
+public class Comment {
+    public String id;
+    public String content;
+    public String employeeID;
+    public String reportID;
+    public String timestamp;
+}
