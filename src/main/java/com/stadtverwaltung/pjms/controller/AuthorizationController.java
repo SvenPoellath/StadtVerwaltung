25fd49259c715aaecef363283f83cad52c852991
Moritz Schley <moritz.schley@icloud.com>
2022-11-21 12:06:24 +0100

    Prevented npe in auth handling

--- src/main/java/com/stadtverwaltung/pjms/controller/AuthorizationController.java
+++ src/main/java/com/stadtverwaltung/pjms/controller/AuthorizationController.java
@@ -6,7 +6,11 @@ public class AuthorizationController {
     private static HashMap<String,String> sessionMap = new HashMap<>();
 
     public boolean hasAuthorization(String employeeID, String sessionID) {
-            return sessionMap.get(employeeID).equals(sessionID);
+            if (sessionMap.get(employeeID) != null && sessionID != null) {
+                return sessionMap.get(employeeID).equals(sessionID);
+            } else {
+                return false;
+            }
     }
 
     public void addSession(String employeeID, String sessionID) {
