c619515af1e9feb37d106567014e4b237bf1ab45
SvenPoellath <sven.poellath@icloud.com>
2022-12-02 22:05:09 +0100

    comment added to report

--- src/components/globalVariables/Report.js
+++ src/components/globalVariables/Report.js
@@ -1,9 +1,11 @@
 const Report = {
-    id: null,
-    latitude: null,
-    longitude: null,
-    kindOfReport: null,
-    pictureID: null,
-    description: null,  
-}
-export default Report
+  id: null,
+  latitude: null,
+  longitude: null,
+  kindOfReport: null,
+  pictureID: null,
+  description: null,
+  comment: null,
+  status: null,
+};
+export default Report;
